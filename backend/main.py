4e3948151dbf07fdd521776a444053b85920e549
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2023-11-23 10:25:51 -0800

    Deleted unnecessary components and endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 import pandas as pd
 from dnn import run_dnn_model
 
 app = FastAPI()
-
 
 # FastAPI server using port 8000 while react app using port 3000
 # If your React app is served from a different port or domain than your FastAPI server, 
@@ -26,45 +24,6 @@ app.add_middleware(
 def read_root():
     return {"message": "Hello from FastAPI"}
 
-# Run "curl http://localhost:8000/example" in terminal to test
-@app.get("/example") # route decorator
-def read_example():
-    return {"message69": "This is another example endpoint"}
-
-# Run 
-# " curl -X POST http://localhost:8000/submit-data -H "Content-Type: application/json" -d '{"key1":"value1", "key2":"value2"}' "
-# -X specifies whether it's GET or POST request (no need for GET b/c it's GET by default)
-# -H specifies the type of content you are sending. You should always specify as json even though
-# it is usually the default for a lot of APIs
-# -d specifies the actual content you are sending (json string in this case). You need to wrap it
-# around single quotes
-@app.post("/submit-data")
-def handle_data(item: dict):
-    # Process the item here
-    return {"received_data": item}
-
-
-# Define a Pydantic model for the User data
-class User(BaseModel):
-    name: str
-    email: str
-    age: int
-
-# 1) Run " curl -X POST http://localhost:8000/create-user -H "Content-Type: application/json" -d '{"name": "Roger", "email": "roger@email.com", "age": 21}' "
-# for successful message confirmation.
-# 2) Run " curl -X POST http://localhost:8000/create-user -H "Content-Type: application/json" -d '{"name": "Lil Tay", "email": "tay@email.com", "age": 11}' "
-# to raise http exception (in this case we raise an exception within this file b/c the user's age is too low).
-@app.post("/create-user")
-def create_user(user: User):
-    # Here, you would typically save the user information to a database
-    # For demonstration, we'll just return the user data with a success message
-
-    # Example of a simple validation
-    if user.age < 18:
-        raise HTTPException(status_code=400, detail="Age must be 18 or above")
-
-    return {"message": "User created successfully", "user": user.dict()}
-
 @app.post("/upload-csv")
 async def upload_csv(file: UploadFile = File(...)):
     if file.content_type != 'text/csv':
@@ -75,10 +34,10 @@ async def upload_csv(file: UploadFile = File(...)):
     
     # Process the CSV file (to feed to ML model)
     # Initialize list of dropped features
-    dropped_features = ['Date', 'Symbol',
-                        'Series', 'Trades', 'Turnover',
-                        'Deliverable Volume', '%Deliverble',
-                        'Last', 'VWAP', 'Prev Close']
+    dropped_features = ["Date", "Symbol",
+                        "Series", "Trades", "Turnover",
+                        "Deliverable Volume", "%Deliverble",
+                        "Last", "VWAP", "Prev Close"]
     cleaned_dataset = dataset.drop(dropped_features, axis=1)
 
     # run ML model
