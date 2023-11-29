0039614cf737a031834777edaa16f2e5a09bf4b1
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2023-11-28 20:49:55 -0800

    Added pair plot generator

---
+++ backend/misc_source_code/pair_plots_generator.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def main():
+    try:
+        df = pd.read_csv("../Datasets/COALINDIA.csv")
+        plot = sns.pairplot(df)
+        plt.savefig('pairplot.png')
+    except Exception as e:
+        print("An error occurred:", e)
+
+if __name__ == '__main__':
+    main()
